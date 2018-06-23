c9745364fc222bc1d81fe2e7457ee1950db109d0
pjakku <39350281+pjakku@users.noreply.github.com>
2018-06-23 19:36:05 -0400

    empty

--- src/main/java/org/springframework/templating/StringTemplateLoader.java
+++ src/main/java/org/springframework/templating/StringTemplateLoader.java
@@ -1,18 +1,1 @@
-package org.springframework.templating;
 
-import org.springframework.core.io.Resource;
-
-/**
- * Loads String templates from resource files.
- * @author Keith Donald
- */
-public interface StringTemplateLoader {
-	
-	/**
-	 * Load the StringTemplate from the file-based resource.
-	 * The template text is generally parsed into a structure that is subsequently cached.
-	 * StringTemplate instances returned to callers of this method are prototypes that may be safely modified and used in the current thread of execution.
-	 */
-	StringTemplate loadStringTemplate(Resource resource);
-		
-}
